46c04486c04845940091defe0ecfa019a2e265be
sc30101997 <122119722+sc30101997@users.noreply.github.com>
2023-01-07 02:05:44 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,1 +1,14 @@
+resource "azurerm_sql_server" "sql-server" {
+    name = var.kpmg_database
+    resource_group_name = var.resource_group
+    location = var.location
+    version = var.kpmg_database_version
+    administrator_login = var.kpmg_database_admin
+    administrator_login_password = var.kpmg_database_password
+}
 
+resource "azurerm_sql_database" "kpmg-db" {
+  name                = "kpmg-db"
+  resource_group_name = var.resource_group
+  location            = var.location
+  server_name         = azurerm_sql_server.sql-server.name
