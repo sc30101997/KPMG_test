6ef9602d8b356f0f00af822be31c3ba02f66e99b
sc30101997 <122119722+sc30101997@users.noreply.github.com>
2023-01-10 11:17:53 +0530

    Create variable.tf

---
+++ task1/variable.tf
@@ -0,0 +1,16 @@
+variable "name" {}
+variable "location" {}
+variable "vnetcidr" {}
+variable "websubnetcidr" {}
+variable "appsubnetcidr" {}
+variable "dbsubnetcidr" {}
+variable "web_host_name"{}
+variable "web_username" {}
+variable "web_os_password" {}
+variable "app_host_name"{}
+variable "app_username" {}
+variable "app_os_password" {}
+variable "kpmg_database" {}
+variable "kpmg_database_admin" {}
+variable "kpmg_database_password" {}
+variable "kpmg_database_version" {}
