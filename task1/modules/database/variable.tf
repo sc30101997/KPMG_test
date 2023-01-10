92952dd355ead0f5bee8a6ebc5b4450d89e2ea86
sc30101997 <122119722+sc30101997@users.noreply.github.com>
2023-01-10 11:26:42 +0530

    Create variable.tf

---
+++ task1/modules/database/variable.tf
@@ -0,0 +1,6 @@
+variable "kpmg_database" {}
+variable "resource_group" {}
+variable "location" {}
+variable "kpmg_database_version" {}
+variable "kpmg_database_admin" {}
+variable "kpmg_database_password" {}
